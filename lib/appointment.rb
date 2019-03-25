ce40597e71156c39562c733b3515fdfea6861aca
Mickae Truman <mtruman@nyit.edu>
2019-03-25 12:33:13 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,22 @@
+require_relative "./patient"
+require_relative "./doctor"
+
+class Appointment
+
+attr_accessor :date, :patient, :doctor
+
+@@all = []
+
+def initialize(date, patient, doctor)
+  @date = date
+  @patient = patient
+  @doctor = doctor
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+require_relative "./patient"
+require_relative "./appointment"
+
+class Doctor
+  
+  attr_accessor :name 
+  
+    @@all = []
+  
+def initialize(name)
+  @name = name 
+  @@all << self
+end
+
+def self.all
+  @@all
+end 
+
+def new_appointment(patient, date)
+  Appointment.new(patient, self, date)
+end
+
+def appointments
+  Appointment.all.select {|appointment| appointment.doctor == self}
+end
+
+def patients
+  appointments.map{|appoointment| appointment.patient}
+end
+  
+
+end
