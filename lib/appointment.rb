73ad676509207363ac9f861226f72bd1104dfd5e
Mickae Truman <mtruman@nyit.edu>
2019-03-25 12:36:08 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -7,15 +7,16 @@ attr_accessor :date, :patient, :doctor
 
 @@all = []
 
-def self.all
-  @@all
-end
 
 def initialize(date, patient, doctor)
   @date = date
   @patient = patient
   @doctor = doctor
   @@all << self
+end
+
+def self.all
+  @@all
 end
 
 
